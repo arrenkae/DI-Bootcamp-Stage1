9a86e4759d11327d64375229a6614e1273adbb2c
slayerstokely <stokely@slayers.ru>
2023-11-15 21:04:53 +0200

    updated the final project

--- Final-Project/script.js
+++ Final-Project/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
   $('.materialboxed').materialbox();
+});
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+
+$(document).ready(function(){
+  $('.slider').slider();
 });
